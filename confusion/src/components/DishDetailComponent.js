dc30db926664cc3eb36d86bad327084e58df2a33
Simnan Khan <Simnan.Khan@neudesic.com>
2022-06-13 15:34:08 +0530

    react furthur integration

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,49 +1,162 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem,Button,Row,Label,Col, Modal,ModalHeader,ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, Errors, LocalForm } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+function handleSubmit(values,dishId,postComment) {
+//  console.log("Current state is",JSON.stringify(values));
+//  alert("Current state is"+JSON.stringify(values));
+  postComment(dishId,values.rating,values.author,values.comment)
+  console.log(JSON.stringify(values))
+
+}
+
+
 
 
 function RenderDish ({dish}) {
   return (
-    <div className="col-12 col md-5 m-1">
-      <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
+    <FadeTransform
+    in
+    transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+    }}>
+        <div className="col-12 col md-5 m-1">
+          <Card>
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+         </div>
+    </FadeTransform>
   );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments,dishId,postComment}) {
+  const [isModalOpen, setModal] = React.useState(false);
+  
+  const toggleModal = () => setModal(!isModalOpen);
   if(comments!=null) 
   return (
     <div className="col-12 col-md-5 m-1">
       <h4>Comments</h4>
       <ul className="list-unstyled">
-        {comments.map((comment)=>{
+      <Stagger in>
+      {comments.map((comment)=>{
           return (
+            <Fade in>
             <li key={comment.id}>
               <p>{comment.comment}</p>
               <p>-- {comment.author} , {comment.date}</p>
             </li>
-
+            </Fade>
           );
         })}
+      </Stagger>
+
       </ul>
+      <Button outline onClick={toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalBody>
+        <ModalHeader onClick={toggleModal}>Submit Comment</ModalHeader>
+        <LocalForm onSubmit={(values)=>handleSubmit(values,dishId,postComment)}>
+                            <Row className="form-group">
+                                <Label htmlFor="rating" md={2}>Rating</Label>
+                                <Col md = {10}>
+                                    <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                      <option value="1">1</option>
+                                      <option value="2">2</option>
+                                      <option value="3">3</option>   
+                                      <option value="4">4</option>                                      
+                                      <option value="5">5</option>                                      
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="author" md={2}>Your Name</Label>
+                                <Col md = {10}>
+                                    <Control.text model=".author" id="author" name="author" 
+                                    placeholder="Your Name" 
+                                    className="form-control"
+                                    validators={{
+                                        required,minLength:minLength(3),maxLength:maxLength(10)
+                                    }}                                    
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required:"Required",
+                                            minLength:"Must be greater than 2 characters",
+                                            maxLength:"Must be 15 characters or less"
+                                        }} 
+                                    />                                                                       
+                                </Col>
+                            </Row>
+              <Row className="form-group">
+                                <Label htmlFor="comment" md={2}>Comment</Label>
+                                <Col md = {10}>
+                                    <Control.textarea model=".comment" id="comment" name="comment" 
+                                    rows="12"
+                                    className="form-control"/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:10,offset:2}}>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>      
+
+        </ModalBody>
+      </Modal>
     </div>
   );
   else 
   return (
-    <div></div>
+    <div>
+    </div>
   )
 }
 
+
+
 const DishDetail = (props)=>{
 
-  if (props.dish != null) 
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+
+  else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  }
+
+  else if (props.dish != null) 
     return (
       <div className="container">
       <div className="row">
@@ -61,10 +174,16 @@ const DishDetail = (props)=>{
               <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-              <RenderComments comments={props.comments} />
+              <RenderComments comments={props.comments} 
+              postComment={props.postComment}
+              dishId={props.dish.id}
+              />
+              <div className="col-12 col-md-5 m-1">
+              </div>
           </div>
+      </div>      
       </div>
-      </div>
+      
     );
   else
   return(
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card,CardImg,CardImgOverlay,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
 import {Link} from "react-router-dom";
+import {Loading} from "./LoadingComponent";
+import {baseUrl} from "../shared/baseUrl";
 
 
 function RenderMenuItem({dish}) {
@@ -8,7 +10,7 @@ function RenderMenuItem({dish}) {
   return (
     <Card>
       <Link to = {`/menu/${dish.id}`} >
-      <CardImg width="80%" src={dish.image} alt={dish.name} />
+      <CardImg width="80%" src={baseUrl + dish.image} alt={dish.name} />
       <CardImgOverlay>
         <CardTitle>{dish.name}</CardTitle>
       </CardImgOverlay>
@@ -19,7 +21,7 @@ function RenderMenuItem({dish}) {
 }
 
 const Menu = (props)=>{
-  const menu = props.dishes.map((dish) => {
+  const menu = props.dishes.dishes.map((dish) => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
         <RenderMenuItem dish={dish} />
@@ -27,10 +29,32 @@ const Menu = (props)=>{
     );
   });
 
+
+  if (props.dishes.isLoading) {
     return (
       <div className="container">
         <div className="row">
-          <Breadcrumb><Link to={"/home"}>Home</Link>
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+
+  else if (props.dishes.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.dishes.errMess}</h4>
+        </div>
+      </div>
+    );
+  }
+
+  else
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb><Link to="/home">Home</Link>
           <BreadcrumbItem active>Menu</BreadcrumbItem>
           </Breadcrumb>
           <div className="col-12">
